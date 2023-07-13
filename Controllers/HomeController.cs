bd29b0dd98452048a6d92e4df5975594728ffee5
Mahsa Kaveh <MaKaveh@schueler.berufskolleg.de>
2023-07-13 09:07:47 +0200

    create

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using TODO.Models;
+
+namespace TODO.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public void Insert(TodoItem todo)
+    {
+        using (SqliteConnection con = 
+            new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+    }//ta alan fght create krdim. read update delete mund (CRUD)
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TODO.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; } // primary key    
+    public string? Name { get; set; }
+}
+
+
